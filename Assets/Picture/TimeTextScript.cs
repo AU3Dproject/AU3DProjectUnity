618d9e02703e36ec9bd89ecb5015fd654b1d07c6
DESKTOP\fuyuk <fuyuki19315@gmail.com>
2016-09-22 16:34:39 +0900

    TimeTextの仕様変更　TalkEventの開発更新　TextWindowの作成　フォントの追加

--- Assets/Picture/TimeTextScript.cs
+++ Assets/Picture/TimeTextScript.cs
@@ -6,6 +6,9 @@ using System;
 public class TimeTextScript : MonoBehaviour {
 
     private Text timeText = null;
+    private float time;
+    private string coron;
+    private string secondBuf = ":";
 
 	// Use this for initialization
 	void Start () {
@@ -15,10 +18,18 @@ public class TimeTextScript : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         System.DateTime t = System.DateTime.Now;
-        timeText.text = t.Year + "年" + t.Month + "月" + t.Day + "日" + "(" + dow(t.DayOfWeek.ToString()) + ")" + t.Hour + "時" + t.Minute + "分" + t.Second + "秒";
+        time += Time.deltaTime;
+
+        if(secondBuf != t.Second.ToString()) {
+            if (coron == ":") coron = " ";
+            else coron = ":";
+        }
+
+        timeText.text = String.Format("{0:0000}" + "/" + "{1:00}" + "/" + "{2:00}" + " " + dowEnglish(t.DayOfWeek.ToString()) + " " + "{3:00}" + coron + "{4:00}" + coron + "{5:00}", t.Year,t.Month,t.Day,t.Hour, t.Minute, t.Second);
+        secondBuf = t.Second.ToString();
     }
 
-    string dow(string dow) {
+    string dowKanji(string dow) {
         switch (dow) {
             case "Sunday":
                 return "日";
@@ -34,6 +45,26 @@ public class TimeTextScript : MonoBehaviour {
                 return "金";
             case "Saturday":
                 return "土";
+        }
+        return "null";
+    }
+
+    string dowEnglish(string dow) {
+        switch (dow) {
+            case "Sunday":
+                return "Sun";
+            case "Monday":
+                return "Mon";
+            case "Tuesday":
+                return "Tue";
+            case "Wednesday":
+                return "Wed";
+            case "Thursday":
+                return "Thu";
+            case "Friday":
+                return "Fri";
+            case "Saturday":
+                return "Sat";
         }
         return "null";
     }
--- Assets/Scripts/TalkEventScript.cs
+++ Assets/Scripts/TalkEventScript.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour {
 
+    [SerializeField]
+    public string text = "";
+    public bool isTextVisible;
+    public float textVisibleFrame = 30.0f;
+
+    private float time;
+    private Text textComponent;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        textComponent = transform.FindChild("Text").GetComponent <Text> ();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        time += Time.fixedDeltaTime;
+
+        if(time % textVisibleFrame > textVisibleFrame) {
+
+        }
 	}
 }
